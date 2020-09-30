56e96a165dc04c5f501410c3d58484e26195ae00
ZeenathMY <zeenath.jjbytes@gmail.com>
2020-09-30 15:53:45 +0530

    added some changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/*const fs = require('fs');
 let file_detailsSync=fs.readdirSync('./');
 console.log(file_detailsSync);
 let file_detailsAsync=fs.readdir('./',function (){
@@ -7,4 +7,23 @@ let file_detailsAsync=fs.readdir('./',function (){
 let file_detailsAsync=fs.readdir('./',function (){
     console.log('within call back');
 });
-console.log(file_detailsAsync);
+console.log(file_detailsAsync);*/
+
+/*
+const path = require('path');
+let x=path.parse(__filename);
+console.log(x);
+let y=path.dirname('/foo/bar/baz/asdf/quux');
+console.log(y);*/
+
+function myTest(message)
+{
+    console.log(message);
+    setTimeout(myTest1,20000);
+    console.log('hello');
+}
+function myTest1()
+{
+    console.log('Hi');
+}
+myTest('Hi I Am John Doe ');
