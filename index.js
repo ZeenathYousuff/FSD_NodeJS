c82b9483a008bcfa03864075adf71110cb5d7584
ZeenathMY <zeenath.jjbytes@gmail.com>
2020-09-30 16:59:46 +0530

    added new os functions

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ let hostName=os.hostname();
 let freeMem=os.freemem();
 let homeDir=os.homedir()
 let networkInterfaces=os.networkInterfaces();
+let version=os.version();
+let type=os.type();
+let time=os.uptime();
 
 console.log(`${release} ${platform} ${arc} ${hostName}`);
-console.log(`${freeMem} ${homeDir} ${networkInterfaces}`);
+console.log(`${freeMem} ${homeDir} ${networkInterfaces} ${version} ${type} ${time}`);
 
