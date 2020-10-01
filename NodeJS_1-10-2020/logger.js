a005032c29529da41bd165f7ed30a38d0383cd48
ZeenathYousuff <32483580+ZeenathYousuff@users.noreply.github.com>
2020-10-01 10:56:41 +0530

    Handling events using inheritance

--- NodeJS_1-10-2020/logger.js
+++ NodeJS_1-10-2020/logger.js
@@ -1,4 +1,4 @@
-const EventEmitter = require('events');
+/*const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 function log(message){
@@ -6,4 +6,23 @@ function log(message){
     emitter.emit("messageLogged");
 }
 
-module.exports.log=log;
+module.exports.log=log;*/
+
+/**********************USING INHERITANCE**************/
+
+const EventEmitter = require('events');
+class Logger extends EventEmitter
+{
+    log(message) {
+        console.log(message)
+        this.emit('messageLogged');
+    }
+    log1(message)
+    {
+        console.log(message);
+        this.emit('messageLogged1');
+    }
+
+}
+
+module.exports=Logger;
