83b4eee64f805102714de9c6c66b7daaa99285ee
ZeenathMY <zeenath.jjbytes@gmail.com>
2020-10-07 10:57:18 +0530

    added 404 error message

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const bodyParser=require('body-parser');
-const express=require('express');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(express.static('public'))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
-const adminRoutes=require('./route/admin');
-const shopRoutes=require('./route/shop');
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-app.listen(3000,(() => {
-    console.log('listening on http://localhost:3000');
-}));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+app.listen(3000);
