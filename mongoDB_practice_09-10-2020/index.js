2db9233bcf713e66427fbb6294db65e409771dae
ZeenathMY <zeenath.jjbytes@gmail.com>
2020-10-09 10:54:35 +0530

    updating the mongooseDB

--- mongoDB_practice_09-10-2020/index.js
+++ mongoDB_practice_09-10-2020/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/play-ground', {useNewUrlParser: true, useUnifiedTopology: true});
 
-
+//By importing the js file in the mongoose DB and accesing it here
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
@@ -16,11 +16,12 @@ const Course = mongoose.model('Course', courseSchema);
 async function getCourses() {
     return Course
         .find({isPublished: true})
-        .and([{tags: 'backend'}])
+        //.and([{tags: 'backend'},{tags: 'frontend'}])
         .sort('-price')
-        .where('price').gte(15)
-        .or({ name: { $regex: /.js/ } })
-.select('name author price tags isPublished');
+        .or({ name: { $regex: /by/ }},
+            {price : {$gte : 15}},
+            {author : {$regex :  /^M/}})
+        .select('name author price tags isPublished');
 }
 
 async function run() {
