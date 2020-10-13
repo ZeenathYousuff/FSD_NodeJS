1199d9e8ee2ec6e253310b1c7992f3150a32addb
ZeenathMY <zeenath.jjbytes@gmail.com>
2020-10-13 10:13:32 +0530

    added pug

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.set('view engine', 'pug');
-app.set('views','views');
+app.set('views','subviews');
 
 app.use((req, res, next) => {
     //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.render('404',{pageTitle: 'error page'});
+    res.render('error',{pageTitle: 'error page'});
 });
 const port=process.env.PORT || 3000
 app.listen(port,() => {
