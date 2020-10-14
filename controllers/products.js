b12a63729a809d9963f3f59838106a5f998e2743
ZeenathMY <zeenath.jjbytes@gmail.com>
2020-10-14 15:22:55 +0530

    added model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,7 @@
-const products = [];
+
+/*****************************************************************/
+//Without Model
+/*const products = [];
 exports.getAddProduct=(req, res)=>
 {
     res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
@@ -8,6 +11,29 @@ exports.postAddProduct = (req, res) => {
     res.redirect('/');
 };
 exports.getProducts = (req, res) => {
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+};
+
+exports.getHome = (req, res) => {
+    res.render('home', {pageTitle: 'Home', path: '/'});
+};*/
+/***************************************************************/
+
+/*************************************************************/
+//With model--> product.js
+
+const Product = require('../models/product');
+exports.getAddProduct=(req, res)=>
+{
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+}
+exports.postAddProduct = (req, res) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+};
+exports.getProducts = (req, res) => {
+    const products = Product.fetchAll();
     res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 };
 
