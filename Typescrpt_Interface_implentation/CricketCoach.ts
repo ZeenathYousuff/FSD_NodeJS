3753812514396b32a6ceda6a97e078d7fb5f8682
ZeenathYousuff <32483580+ZeenathYousuff@users.noreply.github.com>
2020-10-22 17:44:08 +0530

    interface implementation

---
+++ Typescrpt_Interface_implentation/CricketCoach.ts
@@ -0,0 +1,12 @@
+import {Coach} from './Coach';
+import{FortuneService} from './FortuneService';
+export class CricketCoach implements Coach, FortuneService{
+    getDailyWorkOut():string
+    {
+        return "Practice Your Spin Bowling Technique Today";
+    }
+    getDailyFortune():string
+    {
+        return 'today is your lucky day';
+    }
+}
